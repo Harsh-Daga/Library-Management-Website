4e40eb6e82e329c1c2a8671d2663945d29cb69f7
Harsh <64948227+Harsh-Daga@users.noreply.github.com>
2022-03-28 12:24:20 +0530

    Add files via upload

---
+++ admin/registration.php
@@ -0,0 +1,127 @@
+<?php
+  include "connection.php";
+  include "navbar.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+
+  <title>Admin Registration</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+
+  <style type="text/css">
+     @import url('https://fonts.googleapis.com/css2?family=Paytone+One&display=swap');
+    *{
+      font-family: 'Paytone One', sans-serif;
+    }
+
+    section
+    {
+      margin-top: -50px;
+    }
+
+    .box2
+    {
+      height: 540px;
+      width: 540px;
+      background-color: black;
+      margin: 70px auto;
+      opacity: .9;
+      color: white;
+      font-family: 'Paytone One', sans-serif;
+      padding: 22px;
+    }
+
+    .reg_img
+    {
+      height: 610px;
+      margin-top: 15px;
+      background-image: url(images/6.jpg) ;
+    }
+
+    form .login
+    {
+      margin: auto 50px;
+    }
+    input
+    {
+      height: 25px;
+      width: 300px;
+
+    } 
+  </style>   
+</head>
+<body>
+
+<section>
+  <div class="reg_img">
+
+    <div class="box2">
+        <h1 style="text-align: center; font-size: 35px;">User Registration Form</h1>
+        <br>
+
+      <form name="Registration" action="" method="post">
+        
+        <div class="login">
+          <input class="form-control" type="text" name="first" placeholder="First Name" required=""> <br>
+          <input class="form-control" type="text" name="last" placeholder="Last Name" required=""> <br>
+          <input class="form-control" type="text" name="username" placeholder="Username" required=""> <br>
+          <input class="form-control" type="password" name="password" placeholder="Password" required=""> <br>
+          <input class="form-control" type="text" name="email" placeholder="Email" required=""><br>
+          <input class="form-control" type="text" name="contact" placeholder="Phone No" required=""><br>
+
+          <input class="btn btn-default" type="submit" name="submit" value="Sign Up" style="color: black; width: 70px; height: 40px; margin-left: 150px; padding: 2px;"> </div>
+      </form>
+     
+    </div>
+  </div>
+</section>
+
+    <?php
+
+      if(isset($_POST['submit']))
+      {
+        $count=0;
+        $sql="SELECT username from `admin`";
+        $res=mysqli_query($db,$sql);
+
+        while($row=mysqli_fetch_assoc($res))
+        {
+          if($row['username']==$_POST['username'])
+          {
+            $count=$count+1;
+          }
+        }
+        if($count==0)
+        {
+          mysqli_query($db,"INSERT INTO `admin` VALUES('', '$_POST[first]', '$_POST[last]', '$_POST[username]', '$_POST[password]', '$_POST[email]', '$_POST[contact]', 'p.jpg');");
+        ?>
+          <script type="text/javascript">
+           alert("Registration successful");
+          </script>
+        <?php
+        }
+        else
+        {
+
+          ?>
+            <script type="text/javascript">
+              alert("The username already exist.");
+            </script>
+          <?php
+
+        }
+
+      }
+
+    ?>
+
+</body>
+</html>
